4ca77cb36c484e80d5c8c0f635e1ece44fdcd031
Tommy Hodgins <tomhodgins@gmail.com>
2018-04-13 20:32:51 -0400

    Refactoring to use reduce()

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 export default (selector, rule) => {
 
-  let styles = ''
-  let count = 0
+  return Array.from(document.querySelectorAll(selector))
 
-  document.querySelectorAll(selector).forEach(tag => {
+   .reduce((styles, tag, count) => {
 
-    rule.replace(/--([^;]+):(.+)[;}]*/gm, (string, property, value) => {
+      rule.replace(/--([^;]+):(.+)[;}]*/gm, (string, property, value) => {
 
-      tag.setAttribute(`data-${property}`, value)
+        tag.setAttribute(`data-${property}`, value)
 
-    })
+      })
 
-    const attr = selector.replace(/\W+/g, '')
-    const evaluated = rule.replace(/var\(--([^)]+)\)/g, (string, match) => {
+      const attr = selector.replace(/\W+/g, '')
+      const evaluated = rule.replace(/var\(--([^)]+)\)/g, (string, match) => {
 
-      if (tag.getAttribute(`data-${match}`) !== null) {
+        if (tag.getAttribute(`data-${match}`) !== null) {
 
-        return tag.getAttribute(`data-${match}`)
+          return tag.getAttribute(`data-${match}`)
 
-      } else if (tag.closest(`[data-${match}]`) && tag.closest(`[data-${match}]`).getAttribute(`data-${match}`) !== null) {
+        } else if (
+          tag.closest(`[data-${match}]`)
+          && tag.closest(`[data-${match}]`).getAttribute(`data-${match}`) !== null
+        ) {
 
-        return tag.closest(`[data-${match}]`).getAttribute(`data-${match}`)
+          return tag.closest(`[data-${match}]`).getAttribute(`data-${match}`)
 
-      } else {
+        } else {
 
-        if (match in window) {
+          if (match in window) {
 
-          return (new Function(`return ${match}`))() || ''
+            return (new Function(`return ${match}`))() || ''
+
+          }
 
         }
 
-      }
+      })
 
-    })
+      tag.setAttribute(`data-variable-${attr}`, count)
+      styles += `[data-variable-${attr}="${count}"] { ${evaluated} }\n`
+      count++
 
-    tag.setAttribute(`data-variable-${attr}`, count)
-    styles += `[data-variable-${attr}="${count}"] { ${evaluated} }\n`
-    count++
+      return styles
 
-  })
-
-  return styles
+    }, '')
 
 }
