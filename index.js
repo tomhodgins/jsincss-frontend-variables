f43c25ff4e8197a6e344870349113e365719bdf2
Tommy Hodgins <tomhodgins@gmail.com>
2018-12-24 15:52:00 -0500

    Refactor to be cleaner/faster

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 module.exports = (selector, rule) => {
-
-  return Array.from(document.querySelectorAll(selector))
-
-    .reduce((styles, tag, count) => {
-
+  const attr = selector.replace(/\W/g, '')
+  const result = Array.from(document.querySelectorAll(selector))
+    .reduce((output, tag, count) => {
       rule.replace(/--([^;]+):(.+)[;}]*/gm, (string, property, value) => {
-
         tag.setAttribute(`data-${property}`, value)
-
       })
-
       const evaluated = rule.replace(/var\(--([^)]+)\)/g, (string, match) => {
-
         if (tag.getAttribute(`data-${match}`) !== null) {
-
           return tag.getAttribute(`data-${match}`)
-
         } else if (
           tag.closest(`[data-${match}]`)
           && tag.closest(`[data-${match}]`).getAttribute(`data-${match}`) !== null
         ) {
-
           return tag.closest(`[data-${match}]`).getAttribute(`data-${match}`)
-
         } else {
-
           if (match in window) {
-
             return (new Function(`return ${match}`))() || ''
-
           }
-
         }
-
       })
-
-      const attr = selector.replace(/\W/g, '')
-
-      tag.setAttribute(`data-variable-${attr}`, count)
-      styles += `[data-variable-${attr}="${count}"] { ${evaluated} }\n`
-      return styles
-
-    }, '')
-
+      output.add.push({tag: tag, count: count})
+      output.styles.push(`[data-variable-${attr}="${count}"] { ${evaluated} }`)
+      return output
+    }, {add: [], remove: [], styles: []})
+  result.add.forEach(tag => tag.tag.setAttribute(`data-variable-${attr}`, tag.count))
+  result.remove.forEach(tag => tag.setAttribute(`data-variable-${attr}`, ''))
+  return result.styles.join('\n')
 }
